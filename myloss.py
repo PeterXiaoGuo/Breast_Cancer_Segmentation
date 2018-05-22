63db73d33a801be0cfaf9a4f006040de8e6bdd12
Henning <h.buens@gmx.net>
2018-05-22 17:20:49 +0200

    Update myloss.py

--- myloss.py
+++ myloss.py
@@ -1,45 +1,19 @@
-#
-# myloss.py : implementation of the Dice coeff and the associated loss
-#
+'''
+def dice_loss(input, target):
 
-import torch
-from torch.autograd import Function, Variable
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
 
-
-class DiceCoeff(Function):
-    """Dice coeff for individual examples"""
-
-    def forward(self, input, target):
-        self.save_for_backward(input, target)
-        self.inter = torch.dot(input, target) + 0.0001
-        self.union = torch.sum(input) + torch.sum(target) + 0.0001
-
-        t = 2 * self.inter.float() / self.union.float()
-        return t
-
-    # This function has only a single output, so it gets only one gradient
-    def backward(self, grad_output):
-
-        input, target = self.saved_variables
-        grad_input = grad_target = None
-
-        if self.needs_input_grad[0]:
-            grad_input = grad_output * 2 * (target * self.union + self.inter) \
-                         / self.union * self.union
-        if self.needs_input_grad[1]:
-            grad_target = None
-
-        return grad_input, grad_target
+    return (2 * intersection) / (iflat.sum() + tflat.sum())
+'''
 
 
-def dice_coeff(input, target):
-    """Dice coeff for batches"""
-    if input.is_cuda:
-        s = Variable(torch.FloatTensor(1).cuda().zero_())
-    else:
-        s = Variable(torch.FloatTensor(1).zero_())
-
-    for i, c in enumerate(zip(input, target)):
-        s = s + DiceCoeff().forward(c[0], c[1])
-
-    return s / (i + 1)
+def dice_loss(input,target):
+    # target.size() -> [n, h, w] \ in {0, 1}
+    print(target.size()) # [384, 384] \ in {0, 1}
+    inter = (input * target).sum(-1).sum(-1)
+    union = input.sum(-1).sum(-1) + target.sum(-1).sum(-1)
+    result = (2 * inter / union).mean()
+    print('Result: ' + str(result))
+    return result
